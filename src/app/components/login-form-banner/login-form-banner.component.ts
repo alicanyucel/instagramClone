5df9d20f6366870384878138a2716460ecf52baf
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-19 15:42:47 +0300

    tamamlandı.

--- src/app/components/login-form-banner/login-form-banner.component.ts
+++ src/app/components/login-form-banner/login-form-banner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-login-form-banner',
   templateUrl: './login-form-banner.component.html',
   styleUrls: ['./login-form-banner.component.css'],
+  imports:[CardModule],
   standalone:true
 })
 export default class LoginFormBannerComponent {
--- src/app/components/login-form-footer/login-form-footer.component.ts
+++ src/app/components/login-form-footer/login-form-footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-login-form-footer',
   templateUrl: './login-form-footer.component.html',
   styleUrls: ['./login-form-footer.component.css'],
+  imports:[CardModule],
   standalone:true
 })
 export default class LoginFormFooterComponent {
