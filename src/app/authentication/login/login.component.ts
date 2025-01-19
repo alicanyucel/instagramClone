942f4d75eb914b5432aa85062fb441a98901c91e
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-19 15:17:58 +0300

    TEST

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { LoginFormComponent } from 'src/app/components';
-
+import { LoginFormBannerComponent, LoginFormComponent, LoginFormFooterComponent } from 'src/app/components';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  standalone:true,
-  imports:[LoginFormComponent]
+  standalone: true,
+  imports: [LoginFormComponent, LoginFormBannerComponent, LoginFormFooterComponent, InputTextModule]
 })
 export default class LoginComponent {
 
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import {DividerModule} from 'primeng/divider';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
   standalone: true,
-  imports: [CardModule]
+  imports: [CardModule,InputTextModule,CommonModule,ButtonModule,DividerModule]
 })
 export default class LoginFormComponent {
 
