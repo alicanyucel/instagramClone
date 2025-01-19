97400b9db347314cd35367213c78190be2eca167
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-19 13:44:46 +0300

    hata cozuldu

--- src/app/components/login-form-banner/login-form-banner.component.ts
+++ src/app/components/login-form-banner/login-form-banner.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-form-banner',
   templateUrl: './login-form-banner.component.html',
-  styleUrls: ['./login-form-banner.component.css']
+  styleUrls: ['./login-form-banner.component.css'],
+  standalone:true
 })
 export default class LoginFormBannerComponent {
   
--- src/app/components/login-form-footer/login-form-footer.component.ts
+++ src/app/components/login-form-footer/login-form-footer.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-form-footer',
   templateUrl: './login-form-footer.component.html',
-  styleUrls: ['./login-form-footer.component.css']
+  styleUrls: ['./login-form-footer.component.css'],
+  standalone:true
 })
 export default class LoginFormFooterComponent {
 
